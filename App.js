e975633142923afa1f64ea38f7766a76643c8f51
peddirajug <peddiraju.g@robosoftin.com>
2022-04-29 11:25:24 +0530

    Worked on Flat and section list components

--- App.js
+++ App.js
@@ -1,29 +1,80 @@
 import React, { useState } from 'react';
 import {
-  StyleSheet, View, Text, ScrollView, RefreshControl
+  StyleSheet, View, Text, ScrollView,
+  RefreshControl, FlatList, SectionList, SafeAreaView
 } from 'react-native';
 
 const App = () => {
 
   const [item, setItems] = useState([
-    { key: 1, value: 'item1' },
-    { key: 2, value: 'item2' },
-    { key: 3, value: 'item3' },
-    { key: 4, value: 'item4' },
-    { key: 5, value: 'item5' },
-    { key: 6, value: 'item6' },
-    { key: 7, value: 'item7' },
-    { key: 8, value: 'item8' },
-    { key: 9, value: 'item9' }
+    { value: 'item1' },
+    { value: 'item2' },
+    { value: 'item3' },
+    { value: 'item4' },
+    { value: 'item5' },
+    { value: 'item6' },
+    { value: 'item7' },
+    { value: 'item8' },
+    { value: 'item9' }
   ])
 
   const [isRefreshing, setRefresh] = useState(false)
 
   const onRefreshScroll = () => {
     setRefresh(true)
-    setItems([...item, { key: 123, value: '3748' }])
+    setItems([...item, { value: '3748' }])
     setRefresh(false)
   }
+
+  const DATA = [
+    {
+      title: 'item1',
+      data: ['item 1-1', 'item 1-2', 'item 1-3']
+    },
+    {
+      title: 'item2',
+      data: ['item 2-1', 'item 2-2', 'item 2-3']
+    },
+    {
+      title: 'item3',
+      data: ['item 3-1', 'item 3-2']
+    },
+    {
+      title: 'item4',
+      data: ['item 4-1']
+    },
+    {
+      title: 'item5',
+      data: ['item 5-1', 'item 5-2', 'item 5-3']
+    },
+    {
+      title: 'item6',
+      data: ['item 6-1', 'item 6-2', 'item 6-3']
+    },
+    {
+      title: 'item7',
+      data: ['item 7-1', 'item 7-2', 'item 7-3']
+    },
+  ]
+
+  // const DATA11 = [
+  //   {
+  //     title: 'Title 1',
+  //     data: ['Item 1-1', 'Item 1-2', 'Item 1-3'],
+  //   },
+  //   {
+  //     title: 'Title 2',
+  //     data: ['Item 2-1', 'Item 2-2', 'Item 2-3'],
+  //   },
+  //   {
+  //     title: 'Title 3',
+  //     data: ['Item 3-1'],
+  //   },
+  //   {
+  //     title: 'Title 4',
+  //     data: ['Item 4-1', 'Item 4-2'],
+  //   },
+  // ]
 
   return (
     // <View style={styles.body}>
@@ -55,48 +106,89 @@ const App = () => {
           <Text style={styles.text}>Item9</Text>
         </View> */
 
-    < ScrollView
-      style={styles.body}
-      horizontal={false}
-      refreshControl={
-        <RefreshControl
-          refreshing={isRefreshing}
-          onRefresh={onRefreshScroll}
-          colors={['#F68F33']}
-        />
-      }>
+    // < ScrollView
+    //   style={styles.body}
+    //   horizontal={false}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={isRefreshing}
+    //       onRefresh={onRefreshScroll}
+    //       colors={['#F68F33']}
+    //     />
+    //   }>
 
-      {
-        item.map((item) => {
-          return (
-            <View style={styles.item1} key={item.key} >
-              <Text style={styles.text}>{item.value}</Text>
-            </View>
-          )
-        })
-      }
-    </ScrollView >
+    //   {
+    //     item.map((item) => {
+    //       return (
+    //         <View style={styles.item1} key={item.key} >
+    //           <Text style={styles.text}>{item.value}</Text>
+    //         </View>
+    //       )
+    //     })
+    //   }
+    // </ScrollView >
     // </View >
     //  );
+    // <FlatList
+    //   //numColumns={3}
+    //   //horizontal
+    //   //inverted={true}
+    //   keyExtractor={(item, index) => index}
+    //   data={item}
+    //   renderItem={({ item }) => (
+    //     <View style={styles.item1} >
+    //       <Text style={styles.text}>{item.value}</Text>
+    //     </View>
+    //   )}
+    //   refreshControl={
+    //     <RefreshControl
+    //       refreshing={isRefreshing}
+    //       onRefresh={onRefreshScroll}
+    //       colors={['#F68F33']}
+    //     />
+    //   }
+    // />
+
+    <SectionList
+      style={styles.body}
+      scrollEnabled
+      keyExtractor={(item, index) => index.toString()}
+      sections={DATA}
+      renderItem={({ item }) => (
+        <Text style={styles.text}>{item}</Text>
+      )}
+      renderSectionHeader={({ section }) => (
+        <View style={styles.item1} >
+          <Text style={styles.text}>{section.title}</Text>
+        </View>
+      )}
+    />
+
   )
 };
 
 const styles = StyleSheet.create({
-
+  container: {
+    flex: 1,
+    marginHorizontal: 16
+  },
   body: {
     backgroundColor: '#F678FF',
+    flex: 1
   },
   text: {
     color: '#000000',
     fontSize: 20,
     margin: 20,
+    flex: 1
 
   },
   item1: {
     backgroundColor: '#00F000',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 10
+    margin: 10,
+    flex: 1
   },
 
 });
