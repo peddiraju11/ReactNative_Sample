4cd0bd0f4af9315e474c73631480279009767f8f
peddirajug <peddiraju.g@robosoftin.com>
2022-05-03 12:52:40 +0530

    Worked on on Text input and button types

--- App.js
+++ App.js
@@ -1,14 +1,81 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
-  StyleSheet, View, Text, Button, Linking
+  StyleSheet, View, Text, TextInput, Button,
+  TouchableOpacity, TouchableHighlight, TouchableWithoutFeedback, Pressable
 } from 'react-native';
 
 const App = () => {
+
+  const [name, setName] = useState()
+
+  const [IsSubmitted, setSubmit] = useState(false)
+
+  const onPressHandler = () => {
+
+    setSubmit(true)
+  }
   return (
     <View style={styles.body}>
       <Text style={styles.text}>Hi Team</Text>
-      <Button style={styles.button} title='Click Here' onPress={() => { Linking.openURL("https://www.youtube.com") }}></Button>
-    </View>
+      <TextInput
+        style={styles.input}
+        placeholder='e.g Peddy'
+        onChangeText={(newName) => { setName(newName) }}
+        keyboardType='numeric'
+        maxLength={4}
+        secureTextEntry={true}
+      />
+      {/* <Button
+        title='submit'
+        onPress={onPressHandler}
+        disabled={IsSubmitted}
+      /> */}
+      {/* <TouchableOpacity
+        style={styles.button}
+        onPress={onPressHandler}
+        activeOpacity={0.}
+      >
+        <Text style={styles.text}>{IsSubmitted ? 'clear' : 'submit'}</Text>
+      </TouchableOpacity> */}
+
+      {/* <TouchableHighlight
+        style={styles.button}
+        onPress={onPressHandler}
+        activeOpacity={0.5}
+        underlayColor="#00F4"
+      >
+        <Text style={styles.text}>{IsSubmitted ? 'clear' : 'submit'}</Text>
+      </TouchableHighlight> */}
+
+      {/* <TouchableWithoutFeedback
+        onPress={onPressHandler}
+        activeOpacity={0.5}
+        underlayColor="#00F4"
+      >
+        <View style={styles.button} >
+          <Text style={styles.text}>{IsSubmitted ? 'clear' : 'submit'}</Text>
+        </View>
+      </TouchableWithoutFeedback> */}
+
+      <Pressable
+        onPress={onPressHandler}
+        hitSlop={{ top: 20, bottom: 20, left: 20, right: 29 }}
+        android_ripple={'#F56'}
+      >
+        <View style={styles.button} >
+          <Text style={styles.text}>{IsSubmitted ? 'clear' : 'submit'}</Text>
+        </View>
+      </Pressable>
+
+
+
+
+      {
+        IsSubmitted ?
+          <Text style={styles.text}>You entered name: {name}</Text>
+          : null
+      }
+    </View >
   );
 };
 
@@ -18,15 +85,25 @@ const styles = StyleSheet.create({
     backgroundColor: '#ffffff',
     justifyContent: 'center',
     alignItems: 'center',
-    flex: 1
+
   },
   text: {
     color: '#000000',
     fontSize: 20,
+    margin: 10,
+  },
+  input: {
     margin: 20,
+    borderColor: '#F34555',
+    borderWidth: 2,
+    borderRadius: 5,
+    width: 300
   },
   button: {
-    margin: 20,
+    backgroundColor: '#FDF445',
+    width: 150,
+    height: 50,
+    alignItems: 'center'
   }
 
 });
